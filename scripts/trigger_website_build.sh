e51d76ce03d68d8b84e5f907a0df22412f4fca79
Emmanuel Thomas <emmanuel.thomas@tno.nl>
2019-12-12 10:33:33 -0800

    [CI] fix

--- scripts/trigger_website_build.sh
+++ scripts/trigger_website_build.sh
@@ -2,7 +2,7 @@
 
 TOKEN=$1
 
-curl -v -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $TOKEN" -d '{"request": {"branch": "hugo"}}' https://api.travis-ci.com/repo/Dash-Industry-Forum%2FIdentifiers/requests
+curl -v -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $TOKEN" -d '{"request": {"branch": "hugo"}}' https://api.travis-ci.com/repo/Dash-Industry-Forum%2FDash-Industry-Forum.github.io/requests
 
 res=$?
 if test "$res" != "0"; then
