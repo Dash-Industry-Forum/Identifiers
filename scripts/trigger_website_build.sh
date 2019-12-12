f2776246194bb85c132ba8189dfe1c63bd5dd18c
Emmanuel Thomas <emmanuel.thomas@tno.nl>
2019-12-12 09:46:59 -0800

    [CI] script for travis api call

---
+++ scripts/trigger_website_build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+TOKEN=$1
+
+curl -s -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'Travis-API-Version: 3' -H 'Authorization: token '$TOKEN' -d '{request": {"branch":"master"}}' https://api.travis-ci.com/repo/edrthomas%3F/requests
