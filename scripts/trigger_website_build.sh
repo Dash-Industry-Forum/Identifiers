d05baaf2dc60190c244ba816726b0444c77bd379
Emmanuel Thomas <emmanuel.thomas@tno.nl>
2019-12-12 10:06:44 -0800

    [CI] typo

--- scripts/trigger_website_build.sh
+++ scripts/trigger_website_build.sh
@@ -2,7 +2,7 @@
 
 TOKEN=$1
 
-curl -v -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $TOKEN" -d '{"request": {"branch":"master"}}' https://api.travis-ci.com/repo/edrthomas%3FIdentifiers/requests
+curl -v -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $TOKEN" -d '{"request": {"branch": "master"}}' https://api.travis-ci.com/repo/edrthomas%2FIdentifiers/requests
 
 res=$?
 if test "$res" != "0"; then
