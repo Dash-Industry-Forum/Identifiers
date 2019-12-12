141b89e7889e168476588b9f12734fc570b3cdda
Emmanuel Thomas <emmanuel.thomas@tno.nl>
2019-12-12 10:10:33 -0800

    [CI] hugo branch

--- scripts/trigger_website_build.sh
+++ scripts/trigger_website_build.sh
@@ -2,7 +2,7 @@
 
 TOKEN=$1
 
-curl -v -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $TOKEN" -d '{"request": {"branch": "master"}}' https://api.travis-ci.com/repo/edrthomas%2FIdentifiers/requests
+curl -v -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $TOKEN" -d '{"request": {"branch": "hugo"}}' https://api.travis-ci.com/repo/Dash-Industry-Forum.github.io%2FIdentifiers/requests
 
 res=$?
 if test "$res" != "0"; then
